1cbc9f70c31c42e43e3a4ad43dd915eeac72d942
Thetimii <sagertim02@gmail.com>
2025-06-16 12:55:03 +0200

    Update route.ts

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-05-28.basil',
+  apiVersion: '2024-06-20',
 })
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
